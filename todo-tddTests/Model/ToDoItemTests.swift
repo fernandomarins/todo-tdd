97a52e267d469f8955b6fec3f32a61e5954f3d08
Fernando Marins <f.augustomarins@gmail.com>
2022-06-01 14:39:14 -0300

    first commit

---
+++ todo-tddTests/Model/ToDoItemTests.swift
@@ -0,0 +1,21 @@
+//
+//  ToDoItemTests.swift
+//  todo-tddTests
+//
+//  Created by Fernando Marins on 01/06/22.
+//
+
+import XCTest
+@testable import todo_tdd
+
+class ToDoItemTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+}
